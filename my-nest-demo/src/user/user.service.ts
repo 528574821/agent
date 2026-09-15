3aa5198bf877e7e3adad48085c0388ab255cc6e1
Mr.wen <szw_andy@163.com>
2026-09-15 23:02:04 +0800

    添加创建新用户接口

--- my-nest-demo/src/user/user.service.ts
+++ my-nest-demo/src/user/user.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    create(user: any) {
+        return 'This action adds a new user';
+    }
+}
